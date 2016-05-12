aa973cf6c19ccf8964474e3d22ae3cef84c409ae
Gaurav Shukla <gaurav@gauravshukla.xyz>
2016-05-13 05:27:36 +0530

    new design

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the deathping1994githubioApp
  */
 angular.module('deathping1994githubioApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.testimonials= [{
+    	name: "Ishan Srivastava",
+    	desc: "Mecheye Photography",
+    	quote: "When I first asked Gaurav to make a website for me, I was skeptical of his skills and expected him to take a few weeks to come up with the first draft.\nAfter two days he texted me the website url, it was the first and final draft. I loved it.\nGaurav is truely amazing at what he does!",
+    	img: "https://scontent.fbom1-2.fna.fbcdn.net/v/t1.0-9/13226974_1089088527801528_4966241867578043926_n.jpg?oh=2d38b29945af7a0030fa7eb674588cec&oe=57A256CD"
+    },
+    {
+    	name: "Yash Gupta",
+    	desc: "Android Developer",
+    	quote: "I remeber working with him on Probase, it was our first project togather, The way he architectured the webapp was awesome,I've never seen anyone decouple apps so gracefully. He comes up with ingenious solutions, he's crazy about making things scalable but that's a good thing I guess.",
+    	img: "/images/yash.png"
+    }
     ];
-  });
+      });
--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -1,31 +1,70 @@
 'use strict';
 angular.module('deathping1994githubioApp')
-  .controller('ProjectCtrl', function ($scope,$http) {
-    $scope.projects;
-    $http.get("https://api.github.com/users/deathping1994/repos")
-    .then(function(res){
-      $scope.projects=res.data;
-    });
-    $scope.sortproject = function(project) {
-      // console.log(project.updated_at);
+  .controller('ProjectCtrl', function ($scope) {
 
-        var date = new Date(project.updated_at);
-// console.log(date);
-        return -date;
-    };
-
-    $scope.contributions=[{name:"Air Mozilla",
-                          url:"https://github.com/mozilla/airmozilla",
-                          description:"AirMozilla is the video broadcasting site for the Mozilla project"},
-                          {name:"Apache Concerted",
+    $scope.contributions=[
+                        {
+                          name:"Apache Concerted",
                           url:"https://github.com/apache/incubator-concerted",
-                          description:"Concerted is a next generation big data engine aimed at supporting massive in memory reads for OLAP support.The engine is flexible, with no single point of entry and flexibility to use APIs suited for each use case with full scalability.Concerted allows applications to use the native APIs to have the flexibility to store, access, scale data in memory on demand. No auxillary infrastructure is needed.Concerted implements its own locking manager and transaction manager utilizing the object oriented model,thus greatly simplifying the management of processes like commit and abort, depending a lot on the OS's native support."},
-                          {name:"Mozilla Treeherder",
-                          url:"https://github.com/mozilla/treeherder",
-                          description:"A reporting dashboard & service for managing CI data for commits to Mozilla projects"
-                          },
-                          {name:"Moz-Games",
-                          url:"https://github.com/mozilla/moz-games",
-                          description:"Mozilla Games site"}
+                          description:"Concerted is a next generation big data engine aimed at supporting massive in memory reads for OLAP support.The engine is flexible, with no single point of entry and flexibility to use APIs suited for each use case with full scalability.Concerted allows applications to use the native APIs to have the flexibility to store, access, scale data in memory on demand. No auxillary infrastructure is needed.Concerted implements its own locking manager and transaction manager utilizing the object oriented model,thus greatly simplifying the management of processes like commit and abort, depending a lot on the OS's native support.",
+                        },
+                        {
+                        name:"Mozilla Treeherder",
+                        url:"https://github.com/mozilla/treeherder",
+                        description:"A reporting dashboard & service for managing CI data for commits to Mozilla projects"
+                        },
+                        {name:"Moz-Games",
+                        url:"https://github.com/mozilla/moz-games",
+                        description:"Mozilla Games site"}
                         ];
+    $scope.projects= [
+    {
+      name: "Concerted Website",
+      img: "/images/concerted.png",
+      url: "http://gauravshukla.xyz/concerted-website",
+      code: "https://github.com/apache/incubator-concerted",
+      description: "This is a website I designed for Apache-Concerted Project (now dead).This website renders markdown format and can be update via git, no knowledge of webservers needed.",
+      stack: "Angular JS"
+      },
+      {
+        name: "MechEye Photography",
+        url: "http://mecheyephotography.com/",
+        img: "/images/mecheye.png",
+        description: "A photography portfolio designed to be operated and updated from google drive, no hosting account or expertise needed.",
+        stack: "Angular JS/PHP"
+      },
+      {
+        name: "Poolclues",
+        url: "http://poolclues.gauravshukla.xyz/",
+        img: "/images/poolclues.png",
+        description: "Money Pooling platform I created for Shopclues.com. I was the lead architect and backend developer on this one.",
+        stack: "Python-Flask/Angular JS/PgSQL/Mongo/Redis/Elastic Search",
+        presentation: "https://docs.google.com/presentation/d/1JnFJbZd0viSdxPot3rdIVbo9Cbkib7eaP8YeMrYIrkI/edit?usp=sharing"
+      },
+      {
+        name: "Probase",
+        url: "http://probase.gauravshukla.xyz/",
+        code: "https://github.com/deathping1994/probase",
+        img: "/images/probase.png",
+        description: "Project management system with realtime search and plagairism checking I developed for my college.",
+        stack: "Elastic Search/Mongo/Python-Flask/AngularJs",
+        presentation: "https://docs.google.com/presentation/d/1tjkAgJtFk8eTRpIKxPhASYVxD67BpsNPoQwdhhOWG8M/edit?usp=sharing"
+      },
+      {
+        name: "Parallax",
+        url: "https://github.com/Bhramastra/parallax",
+        code: "https://github.com/Bhramastra/parallax",
+        img: "/images/parallax.png",
+        description: "Python Framework for asynchronous and distributed programming (Buggy and incomplete)",
+        stack: "Python",
+        presentation: "https://docs.google.com/presentation/d/1KruvuHJXD6IgVdbRB0JnlMNmK5dh6lMlHbPOlapwLhY/edit?usp=sharing"
+      },
+      {
+        name: "Cyberroam Brute",
+        url: "https://github.com/deathping1994/Cyberoam-Brute",
+        code: "https://github.com/deathping1994/Cyberoam-Brute",
+        img: "/images/cyberroam.png",
+        description: "A python script to carry out brute force attack on JIIT cyberoam portal.(By Far the most used script I ever wrote)",
+        stack: "Python"
+      }];
   });
